d13fcede6f71fbb5cf12f7f219233198057a53a5
hedieh <heydari.hedieh2680@gmail.com>
2025-02-19 07:36:29 +0000

    generate a css file in dist for all style files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 // import addImage from "./add-image.js";
+import Heading from "./components/heading/heading.js";
 import HelloWorldButton from "./components/hello-world-button/hello-world-button.js";
 
 const helloWorldButton = new HelloWorldButton();
 helloWorldButton.render();
+
+const heading = new Heading();
+heading.render();
 
 // addImage();
