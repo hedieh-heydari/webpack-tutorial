3675becaf0387484060d3ebad14844c744fe94e6
hedieh <heydari.hedieh2680@gmail.com>
2025-02-19 08:13:04 +0000

    clear dist folder and its configurations

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const TerserPlugin = require("terser-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -8,6 +9,10 @@ module.exports = {
     filename: "bundle.[contenthash].js",
     path: path.resolve(__dirname, "./dist"),
     publicPath: "dist/",
+    // clean: {
+    //   dry: true,
+    //   keep: /\.css/,
+    // },
   },
   mode: "none",
   module: {
@@ -46,11 +51,18 @@ module.exports = {
       },
     ],
   },
+
   plugins: [
     new TerserPlugin(),
 
     new MiniCssExtractPlugin({
       filename: "style.[contenthash].css",
+    }),
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: [
+        "**/*",
+        path.join(process.cwd(), "build/**/*"),
+      ],
     }),
   ],
 };
