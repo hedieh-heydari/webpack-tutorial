0dd08b898562b7064ae6c3eda8af40c0fb5c0bfb
hedieh <heydari.hedieh2680@gmail.com>
2025-02-22 14:03:32 +0000

    handle showing generated hashcontetn bundle and css

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import HelloWorldButton from "./components/hello-world-button/hello-world-button
 const helloWorldButton = new HelloWorldButton();
 const heading = new Heading();
 heading.render();
-heading.render();
 helloWorldButton.render();
 
 // addImage();
