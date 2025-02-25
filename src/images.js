c6f3bfa308932f2d456908f73f44fab22306d59b
hedieh <heydari.hedieh2680@gmail.com>
2025-02-25 11:01:16 +0000

    config for multi pages with express

--- src/images.js
+++ src/images.js
@@ -1,6 +1,5 @@
 import Heading from "./components/heading/heading.js";
 import Image from "./components/imageComponent/imageComponent.js";
-import React from "react";
 
 const heading = new Heading();
 const image = new Image();
--- src/server.js
+++ src/server.js
@@ -3,12 +3,22 @@ const app = express();
 const path = require("path");
 const fs = require("fs");
 
-app.get("/", function (req, res) {
-  //   res.send("some dummy content");
-  const pathToHtmlFile = path.resolve(__dirname, "../dist/index.html");
+app.get("/hello-world/", function (req, res) {
+  const pathToHtmlFile = path.resolve(__dirname, "../dist/hello-world.html");
   const contentFormatFile = fs.readFileSync(pathToHtmlFile, "utf-8");
   res.send(contentFormatFile);
 });
+
+app.get("/images/", function (req, res) {
+  const pathToHtmlFile = path.resolve(
+    __dirname,
+    "../dist/image-component.html"
+  );
+  const contentFormatFile = fs.readFileSync(pathToHtmlFile, "utf-8");
+  res.send(contentFormatFile);
+});
+
+app.use("/static", express.static(path.resolve(__dirname, "../dist")));
 
 app.listen(2500, function () {
   console.log("app is running on port 2500");
