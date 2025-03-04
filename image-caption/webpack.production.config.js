bd84b47232d6ea61dcf2c4df03bd4f7bca9123ce
hedieh <heydari.hedieh2680@gmail.com>
2025-03-04 06:58:15 +0000

    make new components and its configs

---
+++ image-caption/webpack.production.config.js
@@ -0,0 +1,81 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { ModuleFederationPlugin } = require("webpack").container;
+
+module.exports = {
+  entry: "./src/image-caption.js",
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "./dist"),
+    publicPath: "http://localhost:9003/",
+  },
+  mode: "production",
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+      minSize: 3000,
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg)$/,
+        type: "asset",
+        parser: {
+          dataUrlCondition: {
+            maxSize: 3 * 1024, //change number from 8(default) to 3
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: "/node_modules/",
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/env"],
+          },
+        },
+      },
+      {
+        test: /\.hbs$/,
+        use: ["handlebars-loader"],
+      },
+    ],
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "style.[contenthash].css",
+    }),
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: [
+        "**/*",
+        path.join(process.cwd(), "build/**/*"),
+      ],
+    }),
+    new HtmlWebpackPlugin({
+      filename: "image-caption.html",
+      title: "image caption!",
+      template: "src/page-template.hbs",
+      description: "image caption",
+    }),
+    new ModuleFederationPlugin({
+      name: "ImageCaprtionApp",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./ImageCaption": "./src/components/image-caption/image-caption.js",
+      },
+    }),
+  ],
+};
