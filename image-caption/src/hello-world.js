c17659aba79577c0087f3900d609100184a823f3
hedieh <heydari.hedieh2680@gmail.com>
2025-03-04 08:26:38 +0000

    add lint config

---
+++ image-caption/src/hello-world.js
@@ -0,0 +1,10 @@
+// import addImage from "./add-image.js";
+import Heading from "./components/heading/heading.js";
+import HelloWorldButton from "./components/hello-world-button/hello-world-button.js";
+
+const helloWorldButton = new HelloWorldButton();
+const heading = new Heading();
+heading.render("hello world");
+helloWorldButton.render();
+
+// addImage();
